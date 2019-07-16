5abb7187bbaf8197aa98587f0c1c9c53ddecb138
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2019-07-16 17:50:49 +0900

    Update the description on Qt5 settings

--- upload-ja.sh
+++ upload-ja.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rsync -rv ../website/_build/html/ja/manuals/latest/ cnoidsrv:/var/www/choreonoid.org/public/ja/manuals/latest/
+rsync -rv ../choreonoid-website/_build/html/ja/manuals/latest/ cnoidsrv:/var/www/choreonoid.org/public/ja/manuals/latest/
+
